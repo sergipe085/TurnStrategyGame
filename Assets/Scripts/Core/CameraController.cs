4a7264c0245e3a264c6713eec12957c1d1e47ba9
Sérgio <71034184+serjolas1@users.noreply.github.com>
2022-07-03 15:08:20 -0300

    camera controller update

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -13,6 +14,16 @@ public class CameraController : MonoBehaviour
     }
 
     private void Update() {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void OnUnitSelectChange(object sender, System.EventArgs args) {
+        Unit selectedUnit = UnitActionSystem.Instance.GetSelectedUnit();
+        transform.position = selectedUnit.transform.position;
+    }
+
+    private void HandleMovement() {
         Vector3 inputMoveDir = Vector3.zero;
         inputMoveDir.z = Input.GetAxisRaw("Vertical");
         inputMoveDir.x = Input.GetAxisRaw("Horizontal");
@@ -22,16 +33,13 @@ public class CameraController : MonoBehaviour
 
         float moveSpeed = 4.0f;
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
 
+    private void HandleRotation() {
         if (Input.GetMouseButton(1))  {
             Vector2 rotationInput;
             rotationInput.x = Input.GetAxis("Mouse X");
             transform.eulerAngles += new Vector3(0f, rotationInput.x, 0f);
         }
-    }
-
-    private void OnUnitSelectChange(object sender, System.EventArgs args) {
-        Unit selectedUnit = UnitActionSystem.Instance.GetSelectedUnit();
-        transform.position = selectedUnit.transform.position;
     }
 }
